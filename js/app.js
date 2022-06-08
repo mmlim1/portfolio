f4fa38b372e9528de98540bd82ae40cee871b3dc
mmlim1 <michaelmlim1@gmail.com>
2022-06-08 14:20:29 -0700

    as enlarge html, css, js logos when mouseover

--- js/app.js
+++ js/app.js
@@ -8,10 +8,16 @@
 
 /*------------------------ Cached Element References ------------------------*/
 const title = document.getElementById('title')
+
 const about = document.getElementById('about-link')
 const contact = document.getElementById('contact-link')
 const resume = document.getElementById('resume-link')
 const work = document.getElementById('work-link')
+
+const htmlLogo = document.getElementById('html')
+const cssLogo = document.getElementById('css')
+const jsLogo = document.getElementById('js')
+
 
 
 /*----------------------------- Event Listeners -----------------------------*/
@@ -28,15 +34,41 @@ title.addEventListener('mouseover', function(evt) {
 about.addEventListener('click', function() {
   document.location.href='#about';
 })
+
 contact.addEventListener('click', function() {
   document.location.href='#contact';
 })
+
 resume.addEventListener('click', function() {
   document.location.href='#resume';
 })
+
 work.addEventListener('click', function() {
   document.location.href='#work';
 })
+
+htmlLogo.addEventListener('mouseover', function(evt) {
+  evt.target.style.height = '200px';
+  setTimeout(function() {
+    evt.target.style.height = '100px';
+  }, 500)
+}, false)
+
+cssLogo.addEventListener('mouseover', function(evt) {
+  evt.target.style.height = '200px';
+  setTimeout(function() {
+    evt.target.style.height = '100px';
+  }, 500)
+}, false)
+
+jsLogo.addEventListener('mouseover', function(evt) {
+  evt.target.style.height = '200px';
+  setTimeout(function() {
+    evt.target.style.height = '100px';
+  }, 500)
+}, false)
+
+
 
 
 
