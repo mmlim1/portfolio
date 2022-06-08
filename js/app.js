af49c0a6adddc52f8140825fb7b74a629009e8a8
mmlim1 <michaelmlim1@gmail.com>
2022-06-08 11:49:32 -0700

    add mouseover event listener to title

--- js/app.js
+++ js/app.js
@@ -7,6 +7,7 @@
 
 
 /*------------------------ Cached Element References ------------------------*/
+const title = document.getElementById('title')
 const about = document.getElementById('about-link')
 const contact = document.getElementById('contact-link')
 const resume = document.getElementById('resume-link')
@@ -14,6 +15,16 @@ const work = document.getElementById('work-link')
 
 
 /*----------------------------- Event Listeners -----------------------------*/
+title.addEventListener('mouseover', function(evt) {
+  evt.target.style.color = 'pink';
+  evt.target.style.fontSize = '100px';
+  setTimeout(function() {
+    evt.target.style.color = 'white';
+    evt.target.style.fontSize = '';  
+  }, 500)
+}, false)
+
+
 about.addEventListener('click', function() {
   document.location.href='#about';
 })
