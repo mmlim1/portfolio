5cf6893b86fc87c2367b635ce64f5774b515bf42
mmlim1 <michaelmlim1@gmail.com>
2022-06-08 11:13:22 -0700

    change about contact resume work to use javascript to navigate

--- js/app.js
+++ js/app.js
@@ -7,12 +7,25 @@
 
 
 /*------------------------ Cached Element References ------------------------*/
-
-
+const about = document.getElementById('about-link')
+const contact = document.getElementById('contact-link')
+const resume = document.getElementById('resume-link')
+const work = document.getElementById('work-link')
 
 
 /*----------------------------- Event Listeners -----------------------------*/
-
+about.addEventListener('click', function() {
+  document.location.href='#about';
+})
+contact.addEventListener('click', function() {
+  document.location.href='#contact';
+})
+resume.addEventListener('click', function() {
+  document.location.href='#resume';
+})
+work.addEventListener('click', function() {
+  document.location.href='#work';
+})
 
 
 
