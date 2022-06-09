d210e76e6213e48fcac6722b44b403e039421285
mmlim1 <michaelmlim1@gmail.com>
2022-06-09 11:03:13 -0700

    add external JS data file

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+import { projectData } from "./project-data.js"
 /*-------------------------------- Constants --------------------------------*/
 
 
@@ -17,6 +18,8 @@ const work = document.getElementById('work-link')
 const htmlLogo = document.getElementById('html')
 const cssLogo = document.getElementById('css')
 const jsLogo = document.getElementById('js')
+
+const cardContainer = document.getElementById('card-container')
 
 
 
@@ -73,5 +76,23 @@ jsLogo.addEventListener('mouseover', function(evt) {
 
 
 /*-------------------------------- Functions --------------------------------*/
+let projectMarkup = projectData.map(project => 
+  `
+  <div class="card" style="width: 18rem;">
+    <img src="${project.image}" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">${project.title}</h5>
+      <p class="card-text">${project.description}</p>
+      <div>
+        <a href="${project.github}" class="btn btn-primary">Github</a>
+        <a href="${project.deployment}" class="btn btn-secondary">Deployment</a>
+      </div>
+    </div>
+  </div>
+`
+).join('')
 
+console.log(projectMarkup)
+
+cardContainer.innerHTML = projectMarkup
 
---
+++ js/project-data.js
@@ -0,0 +1,13 @@
+let projectData = [
+  {
+    title: 'Tic Tac Toe',
+    deployment: 'https://ttt-weekend-mmlim.netlify.app/',
+    image: '/assets/ttt-logo.png',
+    github: 'https://github.com/mmlim1/ttt-weekend',
+    description: 'A classic game of between X and O',
+  },
+]
+
+export {
+  projectData
+}
